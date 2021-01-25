677a2267db2beca44495b73299db9888ff23d2bf
Gregory Thomas <gdenvert13@gmail.com>
2021-01-24 21:20:03 -0500

    Fully implementing backend routes with respective unittests
    Adding categories and questions app route
    
    Adding delete question route
    
    Adding tests for create questions route
    
    Adding remaining endpoints and tests
    
    Removing todo comment
    
    Adding more error handlers
    
    Removing credentials for database
    
    Reducing allowed methods to just GET, POST, DELETE

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,173 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories(self):
+        resp = self.client().get('/categories')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_paginated_questions(self):
+        resp = self.client().get('/questions')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+        self.assertTrue(data['total_questions'])
+    
+    def test_404_questions_page_too_big(self):
+        resp = self.client().get('/questions?page=1000')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_delete_qestion(self):
+        question = Question(question='test delete question',
+                            answer='test delete answer',
+                            category=1, difficulty=1)
+        question.insert()
+
+        resp = self.client().delete(f'/questions/{question.id}')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], str(question.id))
+
+    def test_404_delete_question(self):
+        resp = self.client().delete('/questions/')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_422_delete_question(self):
+        resp = self.client().delete('/questions/1000')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+        self.assertEqual(data['message'], 'Unprocessable Entity')
+
+    def test_create_question(self):
+        question = Question(question='test delete question',
+                            answer='test delete answer',
+                            category=1, difficulty=1)
+
+        prev_total = Question.query.count()
+        resp = self.client().post('/questions', json=question.format())
+        data = json.loads(resp.data)
+        curr_total = Question.query.count()
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertEqual(curr_total, prev_total+1)
+
+        created_question = Question.query.get(data['created'])
+        created_question.delete()
+
+    def test_400_create_question_with_bad_json(self):
+        body = {
+            'question': 'test delete question',
+            'answer': 'test delete answer'
+        }
+
+        prev_total = Question.query.count()
+        resp = self.client().post('/questions', json=body)
+        data = json.loads(resp.data)
+        curr_total = Question.query.count()
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], 'Bad Request')
+        self.assertEqual(prev_total, curr_total)
+
+    def test_search_question(self):
+        body = {
+            'query': 'title',
+        }
+
+        resp = self.client().post('/questions/search', json=body)
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+        questions = data['questions']
+
+        for question in questions:
+            self.assertIn(body['query'], question['question'])
+
+    def test_400_search_question_with_bad_json(self):
+        body = {
+            'no_query': 'wow',
+        }
+
+        resp = self.client().post('/questions/search', json=body)
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], 'Bad Request')
+
+    def test_get_questions_by_category(self):
+        resp = self.client().get('/categories/1/questions')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_400_get_questions_by_category(self):
+        resp = self.client().get('/categories/one/questions')
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], 'Bad Request')
+
+    def test_play_quiz(self):
+        body = {
+            'previous_questions': [],
+            'quiz_category': {'type': 'Science', 'id': 1}
+        }
+
+        resp = self.client().post('/quizzes', json=body)
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+    def test_400_play_quiz(self):
+        body = {
+            'previous_questions': [],
+        }
+
+        resp = self.client().post('/quizzes', json=body)
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 400)
+        self.assertEqual(data['message'], 'Bad Request')
 
 
 # Make the tests conveniently executable
